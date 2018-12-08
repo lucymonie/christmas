4d5d5c23bd660ab9b0eefd7acf0dab4448a4fed9
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2018-12-08 21:47:53 +0000

    Updates

---
+++ precache-manifest.3b5b6ed4fc08f570195bcaf88fd634ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "acbf99f7220fe63f2ce5",
+    "url": "/christmas/static/js/runtime~main.acbf99f7.js"
+  },
+  {
+    "revision": "5752582eab9734d02d26",
+    "url": "/christmas/static/js/main.5752582e.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/christmas/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "5752582eab9734d02d26",
+    "url": "/christmas/static/css/main.3948ce8a.chunk.css"
+  },
+  {
+    "revision": "b784c3ab3faf52b5bc066e71dbcfd5a3",
+    "url": "/christmas/index.html"
+  }
+];
